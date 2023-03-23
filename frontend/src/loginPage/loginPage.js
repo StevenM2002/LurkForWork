8accad0f08c9ae71feb66d017802f27b303bef37
Nemat Bhullar <n.bhullar@student.unsw.edu.au>
2023-03-24 00:56:22 +1100

    Added styling for register and login page

--- frontend/src/loginPage/loginPage.js
+++ frontend/src/loginPage/loginPage.js
@@ -2,26 +2,42 @@ import { changeRoute, doFetch } from "../helpers.js";
 
 export const loginPage = () => {
     // Create elems
+    const body = document.createElement('body')
     const div = document.createElement('div');
     const form = document.createElement('form');
+    const emailDiv = document.createElement('div');
+    const emailLabel = document.createElement('label');
     const emailInput = document.createElement('input');
+    const passDiv = document.createElement('div');
+    const passLabel = document.createElement('label');
     const passInput = document.createElement('input');
     const subBtn = document.createElement('button');
     const title = document.createElement('h1');
     const toRegi = document.createElement('a');
 
     // Add attributes
+    body.className = 'background';
     div.id = 'loginpage';
     title.innerText = 'Login to LurkForWork!';
+    emailDiv.className = 'inputDiv';
+    emailLabel.innerText = 'Email Address';
+    emailLabel.className = 'inputLabel';
     emailInput.type = 'text';
-    emailInput.placeholder = 'email'
+    emailInput.placeholder = 'Example@email.com';
+    emailInput.className = 'username';
+    passDiv.className = 'inputDiv';
+    passLabel.innerText = 'Password';
+    passLabel.className = 'inputLabel';
     passInput.type = 'password';
-    passInput.placeholder = 'password';
+    passInput.placeholder = 'Enter password';
+    passInput.className = 'password';
     subBtn.type = 'submit';
     subBtn.innerText = 'Login now!';
+    subBtn.className = 'subBtn';
     form.className = 'centre-form';
     toRegi.href = '/#register';
     toRegi.innerText = 'Register here!';
+    toRegi.className = 'linkBtn';
 
     // Add event handlers
     const onSubmit = (e) => {
@@ -41,8 +57,11 @@ export const loginPage = () => {
     subBtn.addEventListener('click', onSubmit);
 
     // Connect nodes
-    form.append(emailInput, passInput, subBtn, toRegi);
+    emailDiv.append(emailLabel, emailInput);
+    passDiv.append(passLabel, passInput);
+    form.append(emailDiv, passDiv, subBtn, toRegi);
     div.append(title, form);
+    body.append(div);
 
-    return (div);
+    return (body);
 };
--- frontend/src/registrationPage/registrationPage.js
+++ frontend/src/registrationPage/registrationPage.js
@@ -2,31 +2,55 @@ import { doFetch } from "../helpers.js";
 
 export const registrationPage = () => {
     // Create elems
+    const body = document.createElement('body')
     const div = document.createElement('div');
-    const emailInput = document.createElement('input');
+    const nameDiv = document.createElement('div');
+    const nameLabel = document.createElement('label');
     const nameInput = document.createElement('input');
+    const emailDiv = document.createElement('div');
+    const emailLabel = document.createElement('label');
+    const emailInput = document.createElement('input');
+    const passDiv = document.createElement('div');
+    const passLabel = document.createElement('label');
     const passInput = document.createElement('input');
+    const confPassDiv = document.createElement('div');
+    const confPassLabel = document.createElement('label');
     const confPassInput = document.createElement('input');
     const subBtn = document.createElement('button');
     const form = document.createElement('form');
     const title = document.createElement('h1');
-    const errMsg = document.createElement('h2');
+    // const errMsg = document.createElement('h2');
     const toLogin = document.createElement('a');
 
     // Add attr
-    emailInput.placeholder = 'email';
-    nameInput.placeholder = 'name';
+    body.className = 'background';
+    nameDiv.className = 'inputDiv';
+    nameLabel.innerText = 'Name';
+    nameLabel.className = 'inputLabel';
+    nameInput.placeholder = 'Enter name';
+    emailDiv.className = 'inputDiv';
+    emailLabel.innerText = 'Email Address';
+    emailLabel.className = 'inputLabel';
+    emailInput.placeholder = 'Example@email.com';
+    passDiv.className = 'inputDiv';
+    passLabel.innerText = 'Password';
+    passLabel.className = 'inputLabel';
     passInput.type = 'password';
-    passInput.placeholder = 'password';
+    passInput.placeholder = 'Enter password';
+    confPassDiv.className = 'inputDiv';
+    confPassLabel.innerText = 'Confirm Password';
+    confPassLabel.className = 'inputLabel';
     confPassInput.type = 'password';
-    confPassInput.placeholder = 'confirm password';
+    confPassInput.placeholder = 'Confirm password';
     subBtn.type = 'submit';
     subBtn.textContent = 'Sign up!';
+    subBtn.className = 'subBtn';
     div.id = 'registrationpage';
     form.className = 'centre-form';
     title.innerText = 'Register to LurkForWork!';
     toLogin.href = '/#login';
     toLogin.innerText = 'Login here!';
+    toLogin.className = 'linkBtn';
 
     // Add event handlers
     subBtn.addEventListener('click', (e) => {
@@ -49,7 +73,12 @@ export const registrationPage = () => {
     
     
     // Connect nodes
-    form.append(nameInput, emailInput, passInput, confPassInput, subBtn, toLogin);
-    div.append(title, errMsg, form);
-    return div;
+    nameDiv.append(nameLabel, nameInput);
+    emailDiv.append(emailLabel, emailInput);
+    passDiv.append(passLabel, passInput);
+    confPassDiv.append(confPassLabel, confPassInput);
+    form.append(nameDiv, emailDiv, passDiv, confPassDiv, subBtn, toLogin);
+    div.append(title, form); /* removed errMdg from here */
+    body.append(div);
+    return body;
 };
